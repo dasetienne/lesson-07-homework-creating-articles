81d2580853c7405c5314723c871672bb0122b0e3
dasetienne <etienne.leue@gmail.com>
2016-03-21 16:51:02 -0400

    With indenting fixes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,8 +24,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 
 end
